b7ddab54e8b1300163af4bed2a9839a6687b61bd
guncat-02 <nuit0204@naver.com>
2024-06-07 17:21:39 +0900

    Delete sns directory

--- sns/src/main/java/dao/IF_ProfileDao.java
+++
@@ -1,8 +0,0 @@
-package dao;
-
-import vo.ProfileVO;
-
-public interface IF_ProfileDao {
-	public void insert(ProfileVO pVO) throws Exception;
-	public int chk(String nickName) throws Exception;
-}
--- sns/src/main/java/service/IF_ProfileService.java
+++
@@ -1,8 +0,0 @@
-package service;
-
-import vo.ProfileVO;
-
-public interface IF_ProfileService {
-	public void insert(ProfileVO pVO) throws Exception;
-	public int chk(String nickName) throws Exception;
-}
